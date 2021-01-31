949092a1fad5feb658d2b4fb2b48286d83be2c0c
Mikhail Lazarev <mikael.lazarev@gmail.com>
2021-01-31 22:53:20 +0300

    deploy update

--- scripts/build_dev.sh
+++ scripts/build_dev.sh
@@ -1,4 +1,7 @@
 # Read environment from .env file
+ulimit -m
+ulimit -v
+ulimit -u
 set -o allexport; source ./.env; set +o allexport;
-WASM_BUILD_TOOLCHAIN=nightly-2020-10-06-x86_64-apple-darwin
-cargo +nightly-2020-10-06 run -- --dev --tmp
+WASM_BUILD_TOOLCHAIN=nightly-2020-10-06
+cargo +nightly-2020-10-06 run -- --dev --tmp --unsafe-ws-external --rpc-cors all
